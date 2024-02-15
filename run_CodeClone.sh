73d9110a3c52f9334871ff57bf21f610d3ec3479
FahadEbrahim <62794766+FahadEbrahim@users.noreply.github.com>
2024-02-15 14:43:28 +0000

    Update run_CodeClone.sh

--- run_CodeClone.sh
+++ run_CodeClone.sh
@@ -1,6 +1,6 @@
 python3 train_code_clone.py \
     --model_ckpt microsoft/unixcoder-base-nine \
-    --num_epochs 30 \
+    --num_epochs 1 \
     --batch_size 8 \
     --num_warmup_steps 10 \
     --learning_rate 5e-4 
